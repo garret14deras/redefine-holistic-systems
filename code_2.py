a37e5ca126351676c3b387bf18e1f73a1f8ab8ae
garret14deras <139816612+garret14deras@users.noreply.github.com>
2024-03-08 02:04:48 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,28 @@
+        for (int i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+            System.out.println("Random Number: " + item);
+}
+    }
+        }
+
+        int[] data = new int[10];
+
+public class RandomDataGenerator {
+        for (int item : data) {
+    public static void main(String[] args) {    const data = [];
+}
+    data.forEach(item => {
+
+function main() {
+    });
+
+        console.log("Random Number: " + item);
+main();
+    const data = generateRandomData();
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+function generateRandomData() {
+    }
+    return data;
+}
